3e4332db83d8ad9d7fe8385dea8c82ce57785f9c
ahewardmills76 <ahewardmills76@gmail.com>
2019-07-17 17:24:05 -0500

    updated index.js to create an in-memory instance of mongodb and updated ads.js to include updateAd and deleteAd functions

--- src/index.js
+++ src/index.js
@@ -7,13 +7,13 @@ const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
 
+
 // defining the Express app
 const app = express();
 
-// defining an array to work as the database (temporary solution)
-const ads = [
-  {title: 'Hello, world (again)!'}
-];
+// defining the database
+const {startDatabase} = require('./database/mongo');
+const {insertAd, getAds} = require('./database/ads');
 
 // adding Helmet to enhance your API's security
 app.use(helmet());
@@ -28,11 +28,16 @@ app.use(cors());
 app.use(morgan('combined'));
 
 // defining an endpoint to return all ads
-app.get('/', (req, res) => {
-  res.send(ads);
-});
+app.get('/', async (req, res) => {
+    res.send(await getAds());
+  });
 
-// starting the server
-app.listen(3001, () => {
-  console.log('listening on port 3001');
+  // start the in-memory MongoDB instance
+startDatabase().then(async () => {
+    await insertAd({title: 'Hello, now from the in-memory database!'});
+    
+ // start the server
+ app.listen(3001, async () => {
+    console.log('listening on port 3001');
+  });
 });
